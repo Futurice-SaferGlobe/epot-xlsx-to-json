119d9db8742646f92838c86f826bddd6fdad7ab3
johnzena <manu.suhonen@gmail.com>
2018-10-03 15:57:50 +0300

    Update XLSX to json.py

--- XLSX to json.py
+++ XLSX to json.py
@@ -1,32 +1,36 @@
 import sys
 import xlrd
 
+
 def main():
-    filename ="sdf"
     for arg in sys.argv[1:]:
         filename = arg
-
     book = xlrd.open_workbook(filename)
-    sheetcount = book.nsheets-1
-    f = open("demofile2.json", "w")
-    sheetcount = 2
+    f = open("XLSX_TO_JSON.json", "w")
+    sheetcount = book.nsheets-2
+    f.write("[" +"\n")
+    f.write("    {" +"\n")
+    f.write("    \""+ str(book.sheet_by_index(book.nsheets-2).cell(0,0).value) +"\""+":" +" \"" + str(book.sheet_by_index(book.nsheets-2).cell(0,1).value)+ "\"" + "," + "\n")
+    f.write("    \""+ str(book.sheet_by_index(book.nsheets-2).cell(1,0).value) +"\""+":" +" \"" + str(book.sheet_by_index(book.nsheets-2).cell(1,1).value)+ "\"" + "," + "\n")
+    f.write("    \""+ str(book.sheet_by_index(book.nsheets-2).cell(2,0).value) +"\""+":" +" \"" + str(book.sheet_by_index(book.nsheets-2).cell(2,1).value)+ "\"" + "," + "\n")
+    f.write("    \"data\": [" + "\n")
     for I in range(sheetcount):
         currsheet = book.sheet_by_index(I)
-        f.write("[{")
+        f.write("        {")
         f.write("\n")
-        f.write(" \"index\""+":" +" "+  str(I+1) + ",")
+        f.write("        \"index\""+":" +" "+  str(I+1) + ",")
         f.write("\n")
         #string = string.replace("\n",' ')
-        f.write(" \"title\"" + ":" + " " + " \"" + str(currsheet.cell(0,1).value) + "\"" + ",")
+        f.write("        \"title\"" + ":" + " " + " \"" + str(currsheet.cell(0,1).value) + "\"" + ",")
         f.write("\n")
-        string = " \"content\"" + ": " + " \""+str(currsheet.cell(1,1).value) + "\" " + ","
+        string = "        \"content\"" + ": " + " \""+str(currsheet.cell(1,1).value) + "\" " + ","
         string = string.replace("\n",' ')
         f.write(string)
         f.write("\n")
         labels = list
-        f.write(" \"labels\"" + ": " + "["+ "\"" +str(currsheet.cell(1,2).value) + "\""+ "]")
+        f.write("        \"labels\"" + ": " + "["+ "\"" +str(currsheet.cell(1,2).value) + "\""+ "]"+ ",")
         f.write("\n")
-        f.write(" \"subheaders\"" + ": " + "[")
+        f.write("        \"subheaders\"" + ": " + "[")
         f.write("\n")
         rows = currsheet.nrows
         cols = currsheet.ncols
@@ -35,31 +39,40 @@ def main():
         counter = 0
         for i in range(1,rows-1,2):
             counter = counter+1
-            f.write("   {")
+            f.write("            {")
             f.write("\n")
-            f.write("    \"index\""+":" +" "+  str(counter) + ",")
+            f.write("            \"index\""+":" +" "+  str(counter) + ",")
             f.write("\n")
-            f.write("    \"title\"" + ":" + " " + " \"" + str(currsheet.cell(i+1,1).value) + "\"" + ",")
+            f.write("            \"title\"" + ":" + " " + " \"" + str(currsheet.cell(i+1,1).value) + "\"" + ",")
             f.write("\n")
-            string1 = "    \"content\":" + " \"" + str(currsheet.cell(i+2,1).value) + "\"" + ","
+            string1 = "            \"content\":" + " \"" + str(currsheet.cell(i+2,1).value) + "\"" + ","
             string1 = string1.replace("\n",' ')
             f.write(string1)
             f.write("\n")   
             if currsheet.cell(i+2,2).value == "": 
-                f.write("    \"labels\"" + ": " + "[]")
+                f.write("            \"labels\"" + ": " + "[]")
             else:
-                f.write("    \"labels\"" + ": " + "[" + "\"" + str(currsheet.cell(i+2,2).value) + "\""+ "]")
+                f.write("            \"labels\"" + ": " + "[" + "\"" + str(currsheet.cell(i+2,2).value) + "\""+ "]")
             f.write("\n")
             if i == rows-3:
-                f.write("   }")
+                f.write("            }")
             else:
-                f.write("   },")
+                f.write("            },")
             f.write("\n")
         counter = 0
-        f.write("  ]")
+        f.write("        ]")
         f.write("\n")
-        f.write("},")
+        if I != book.nsheets-3:
+            f.write("        },")
+        if I == book.nsheets-3: 
+            f.write("        }")
         f.write("\n")
+
+    f.write("        ]")
+    f.write("\n")
+    f.write("    }")
+    f.write("\n")
+    f.write("]")
     f.close()
 
 if __name__ == "__main__":
